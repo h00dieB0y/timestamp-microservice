8e0bf92aa88fc5fd066afd9e6daa93ec32c389a1
hoddieBoy <emilemannekitsoukou@gmail.com>
2024-12-14 16:04:21 +0100

    feat: add API endpoint to return Unix timestamp and UTC date

--- index.js
+++ index.js
@@ -24,6 +24,39 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Return Unix timestamp
+app.get('/api/:date?', function (req, res) {
+  let { date } = req.params;
+  let unix;
+  let utc;
+
+  // If date is not provided, return current date
+  if (!date) {
+    let now = new Date();
+    unix = now.getTime();
+    utc = now.toUTCString();
+
+    return res.json({ unix: unix, utc: utc });
+  }
+  
+    // If date is a number, parse it as an integer
+    if (!isNaN(date)) {
+      date = parseInt(date);
+    }
+
+    let parsedDate = new Date(date);
+
+    // If date is invalid, return error
+    if (parsedDate.toString() === 'Invalid Date') {
+      return res.json({ error: 'Invalid Date' });
+    }
+
+    unix = parsedDate.getTime();
+    utc = parsedDate.toUTCString();
+
+    return res.json({ unix: unix, utc: utc });
+});
+
 
 
 // Listen on port set in environment variable or default to 3000
